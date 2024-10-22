ae4d31d79b6e1e196a50b13816babb55b8c6c403
kat2chao <kat2chao@yahoo.com>
2024-10-22 00:53:21 -0500

    made global activity array createEvents

--- Pebble/CreateEvents.swift
+++ Pebble/CreateEvents.swift
@@ -25,18 +25,24 @@ class Event {
     var startTime: Date
     var endTime: Date
     var location: String
-    var activity: String
+    //each event can be sorted by specific activities, ex. fitness and soccer
+    var activities: [String]
     var numPeople: Int
     
-    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activity: String, numPeople: Int) {
+    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, numPeople: Int) {
         self.title = title
         self.description = description
         self.date = date
         self.startTime = startTime
         self.endTime = endTime
         self.location = location
-        self.activity = activity
+        self.activities = []
         self.numPeople = numPeople
+    }
+    
+    func addActivity(_ activity: String) {
+        //Check if activity exists in global list before adding to event
+        
     }
 }
 
@@ -49,4 +55,16 @@ class allActivities {
     
     //other classes cannot directly access this
     private(set) var globalActivities: [String] = ["Hiking", "Running"]
+    
+    //initializer
+    private init() {}
+    
+    func addGlobalActivity(_ activity: String) {
+        if !globalActivities.contains(activity) {
+            globalActivities.append(activity)
+            print("\(activity) has been added to the global list")
+        } else {
+            print("\(activity) already exists in the global list")
+        }
+    }
 }
