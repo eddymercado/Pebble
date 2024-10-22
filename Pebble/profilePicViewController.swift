27e8b5571bf359697795541cce139ecfba8932fb
Denise Ramos <ramosden0313@gmail.com>
2024-10-22 00:25:51 -0500

    profile pic

--- Pebble/profilePicViewController.swift
+++ Pebble/profilePicViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class profilePicViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,10 +17,42 @@ class profilePicViewController: UIViewController {
     }
     
 
+    @IBAction func uploadPhotoButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        present(vc, animated: true)
+    }
+    
     @IBAction func profilePicSaveButton(_ sender: Any) {
         performSegue(withIdentifier: "profilePicToSelectInterests", sender: self)
         }
+    
+    
+    
     }
+
+extension profilePicViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let profilePic = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            
+            imageView.image = profilePic
+            
+        }
+        picker.dismiss(animated: true, completion: nil)
+
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
 
 
 
