8c985ed3f94b9e402afb75e75a7928cbeb4c3cb0
kat2chao <kat2chao@yahoo.com>
2024-10-22 00:29:35 -0500

    made event class in createEvents

--- Pebble/CreateEvents.swift
+++ Pebble/CreateEvents.swift
@@ -17,3 +17,36 @@ class CreateEvents: UIViewController {
     
 
 }
+
+class Event {
+    var title: String
+    var description: String
+    var date: Date
+    var startTime: Date
+    var endTime: Date
+    var location: String
+    var activity: String
+    var numPeople: Int
+    
+    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activity: String, numPeople: Int) {
+        self.title = title
+        self.description = description
+        self.date = date
+        self.startTime = startTime
+        self.endTime = endTime
+        self.location = location
+        self.activity = activity
+        self.numPeople = numPeople
+    }
+}
+
+// Singleton class, global list of activities
+// Each event has its own associated list of activities
+// which pulls from the class
+class allActivities {
+    //single instance
+    static let shared = allActivities()
+    
+    //other classes cannot directly access this
+    private(set) var globalActivities: [String] = ["Hiking", "Running"]
+}
