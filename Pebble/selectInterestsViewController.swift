97e32b8e9292ba1c48b4de898133ea5102bd2f00
Denise Ramos <ramosden0313@gmail.com>
2024-10-21 23:11:21 -0500

    denise 2

--- Pebble/selectInterestsViewController.swift
+++ Pebble/selectInterestsViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class selectInterestsViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +15,10 @@ class selectInterestsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectInterestToBrowseEvents(_ sender: Any) {
+        performSegue(withIdentifier: "selectInterestToBrowseEvents", sender: self)
+        
     }
-    */
+    
 
 }
