541935b877506bf15036e0538f40fa43e8259ef8
Denise Ramos <ramosden0313@gmail.com>
2024-10-21 20:30:05 -0500

    denise adding

---
+++ Pebble/selectInterestsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  selectInterestsViewController.swift
+//  Pebble
+//
+//  Created by Denise Ramos on 10/21/24.
+//
+
+import UIKit
+
+class selectInterestsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
