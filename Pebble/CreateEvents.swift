aeef169bd37d2fda5fd5b38f5b1aca149cc06df6
eddymercado <eddym@utexas.edu>
2024-10-23 17:39:28 -0500

    create events broken debug time

--- Pebble/CreateEvents.swift
+++ Pebble/CreateEvents.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 protocol EventCreationDelegate: AnyObject {
-    func createdEvent(_event: Event)
+    func createdEvent(_ event: Event)
 }
 
 class CreateEvents: UIViewController {
     @IBOutlet weak var eventTitle: UITextField!
-    @IBOutlet weak var eventDesc: UITextView!
+    @IBOutlet weak var eventDesc: UITextField!
     @IBOutlet weak var eventDate: UIDatePicker!
     @IBOutlet weak var eventStartTime: UIDatePicker!
     @IBOutlet weak var eventEndTime: UIDatePicker!
@@ -41,25 +41,30 @@ class CreateEvents: UIViewController {
     
     
     @IBAction func createEventBtnPressed(_ sender: UIButton) {
-        
-        //get selected date and times from date picker
+        // Get selected date and times from date picker
         let selectedDate = eventDate.date
         let selectedStart = eventStartTime.date
         let selectedEnd = eventEndTime.date
-        
-        //new instance of event, populate inputs
-        let newEvent = Event (title: eventTitle.text ?? "", description: eventDesc.text ?? "", date: selectedDate, startTime: selectedStart, endTime: selectedEnd, location: eventLocation.text ?? "", numPeople: Int(eventNumPeople.text ?? "0") ?? 0
+
+        // Create a new instance of Event, populating inputs
+        let newEvent = Event(
+            title: eventTitle.text ?? "",
+            description: eventDesc.text ?? "",
+            date: selectedDate,
+            startTime: selectedStart,
+            endTime: selectedEnd,
+            location: eventLocation.text ?? "",
+            activities: eventActivityType.text ?? "",
+            numPeople: Int(eventNumPeople.text ?? "0") ?? 0
         )
-        
-        //add event to global array
-        eventsList.append(newEvent)
-        
-        //notify delegate new event created
-        delegate?.createdEvent(_event: newEvent)
-        
-        //need to add some navigation controller code to go to a diff VC potentially
+
+        // Notify delegate that a new event was created
+        delegate?.createdEvent(newEvent)
+
+        // Dismiss the view controller
         navigationController?.popViewController(animated: true)
     }
+
 }
 
 extension CreateEvents:UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -86,45 +91,45 @@ class Event {
     var endTime: Date
     var location: String
     //each event can be sorted by specific activities, ex. fitness and soccer
-    var activities: [String]
+    var activities: String
     var numPeople: Int
     
-    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, numPeople: Int) {
+    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activities: String, numPeople: Int) {
         self.title = title
         self.description = description
         self.date = date
         self.startTime = startTime
         self.endTime = endTime
         self.location = location
-        self.activities = []
+        self.activities = activities
         self.numPeople = numPeople
     }
     
-    func addActivity(_ activity: String) {
-        //Check if activity exists in global list before adding to event
-        
-    }
+//    func addActivity(_ activity: String) {
+//        //Check if activity exists in global list before adding to event
+//        
+//    }
 }
 
 // Singleton class, global list of activities
 // Each event has its own associated list of activities
 // which pulls from the class
-class allActivities {
-    //single instance
-    static let shared = allActivities()
-    
-    //other classes cannot directly access this
-    private(set) var globalActivities: [String] = ["Hiking", "Running"]
-    
-    //initializer
-    private init() {}
-    
-    func addGlobalActivity(_ activity: String) {
-        if !globalActivities.contains(activity) {
-            globalActivities.append(activity)
-            print("\(activity) has been added to the global list")
-        } else {
-            print("\(activity) already exists in the global list")
-        }
-    }
-}
+//class allActivities {
+//    //single instance
+//    static let shared = allActivities()
+//    
+//    //other classes cannot directly access this
+//    private(set) var globalActivities: [String] = ["Hiking", "Running"]
+//    
+//    //initializer
+//    private init() {}
+//    
+//    func addGlobalActivity(_ activity: String) {
+//        if !globalActivities.contains(activity) {
+//            globalActivities.append(activity)
+//            print("\(activity) has been added to the global list")
+//        } else {
+//            print("\(activity) already exists in the global list")
+//        }
+//    }
+//}
---
+++ Pebble/EventCell.swift
@@ -0,0 +1,20 @@
+//
+//  EventCell.swift
+//  Pebble
+//
+//  Created by Eddy Mercado on 10/23/24.
+//
+
+
+import UIKit
+
+class EventCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var numPeopleLabel: UILabel!
+    @IBOutlet weak var activitiesLabel: UILabel!
+}
