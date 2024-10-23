e40321f4a2792c6106cff5002ba52a7ff1f284bd
eddymercado <eddym@utexas.edu>
2024-10-22 21:12:46 -0500

    button toggle

--- Pebble/selectInterestsViewController.swift
+++ Pebble/selectInterestsViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class selectInterestsViewController: UIViewController {
+    
+    @IBOutlet weak var toggleSoccerButton: UIButton!
+    var isSelected: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        toggleSoccerButton.backgroundColor = UIColor.systemGray2
     }
     
+    @IBAction func toggleButtonPressed(_ sender: UIButton) {
+            isSelected.toggle()  // Toggle the selected state
+
+        if isSelected {
+            toggleSoccerButton.backgroundColor = UIColor.systemBlue
+        } else {
+            toggleSoccerButton.backgroundColor = UIColor.systemGray2
+        }
+    }
 
     @IBAction func selectInterestToBrowseEvents(_ sender: Any) {
         performSegue(withIdentifier: "selectInterestToBrowseEvents", sender: self)
