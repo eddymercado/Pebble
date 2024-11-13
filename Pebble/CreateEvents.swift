9b2393949b9da53875afd4603d1cb98d6c7aaed1
kat2chao <kat2chao@yahoo.com>
2024-11-13 12:36:03 -0600

    map view worksgit add . git push

--- Pebble/CreateEvents.swift
+++ Pebble/CreateEvents.swift
@@ -16,7 +16,7 @@ protocol EventCreationDelegate: AnyObject {
     func createdEvent(_ event: Event)
 }
 
-class CreateEvents: UIViewController, MKMapViewDelegate {
+class CreateEvents: UIViewController, UITextFieldDelegate, MKMapViewDelegate {
     @IBOutlet weak var eventTitle: UITextField!
     @IBOutlet weak var eventDesc: UITextField!
     @IBOutlet weak var eventDate: UIDatePicker!
@@ -26,12 +26,63 @@ class CreateEvents: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var eventActivityType: UITextField!
     @IBOutlet weak var eventNumPeople: UITextField!
     @IBOutlet weak var eventImageView: UIImageView!
+
+    @IBOutlet weak var mapView: MKMapView!
     
     weak var delegate:EventCreationDelegate?
+    let geocoder = CLGeocoder() //convert address to coordinates
+    var selectedLocation: CLLocationCoordinate2D? //stores coordinates
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        eventLocation.delegate = self
+        mapView.delegate = self
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == eventLocation {
+            searchLocation()
+        }
+    }
+    
+    func searchLocation() {
+        guard let address = eventLocation.text, !address.isEmpty else {return}
+        
+        //convert address string to coordinates
+        geocoder.geocodeAddressString(address) { [weak self] (placemarks, error) in guard let self = self else {return}
+            if let error = error {
+                print("Geocoding error: \(error.localizedDescription)")
+                return
+            }
+            guard let placemark = placemarks?.first, let location = placemark.location
+            else {
+                print("No location found")
+                return
+            }
+            self.selectedLocation = location.coordinate
+            self.updateMap(with: location.coordinate)
+        }
+    }
+    
+    func updateMap(with coordinate: CLLocationCoordinate2D) {
+        //create region centered on location
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+        
+        //create and add annotation for location
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = eventLocation.text
+        //remove existing annotations
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotation(annotation)
+    }
+    
+    func updateMap(with geoPoint: GeoPoint) {
+        let coordinate = CLLocationCoordinate2D(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
+        updateMap(with: coordinate)
+    }
+    
     
     @IBAction func uploadPhotoPressed(_ sender: UIButton) {
         let vc = UIImagePickerController()
@@ -51,7 +102,7 @@ class CreateEvents: UIViewController, MKMapViewDelegate {
         let db = Firestore.firestore()
         guard let userId = Auth.auth().currentUser?.uid else { return }
         
-        db.collection("users").document(userId).getDocument { (document, error) in
+        db.collection("users").document(userId).getDocument(source: .default) { (document, error) in
             if let error = error {
                 print("Error retrieving document: \(error.localizedDescription)")
                 return
@@ -69,7 +120,7 @@ class CreateEvents: UIViewController, MKMapViewDelegate {
             }
             
             // Prepare event data for Firestore
-            let eventData: [String: Any] = [
+            var eventData: [String: Any] = [
                 "title": self.eventTitle.text ?? "",
                 "description": self.eventDesc.text ?? "",
                 "date": self.eventDate.date,
@@ -82,6 +133,11 @@ class CreateEvents: UIViewController, MKMapViewDelegate {
                 "hostPfp": hostPfp,
                 "hostId": userId
             ]
+            
+            //add location coordinates to eventData
+            if let coordinate = self.selectedLocation {
+                eventData["coordinate"] = GeoPoint(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            }
 
             // Add event to "events" collection
             db.collection("events").addDocument(data: eventData) { error in
@@ -107,13 +163,16 @@ class CreateEvents: UIViewController, MKMapViewDelegate {
                         startTime: eventData["startTime"] as! Date,
                         endTime: eventData["endTime"] as! Date,
                         location: eventData["location"] as! String,
+                        coordinate: eventData["coordinate"] as? GeoPoint,
                         activities: eventData["activities"] as! String,
                         numPeople: eventData["numPeople"] as! Int,
                         hostUsername: hostUsername,
                         hostPfp: hostPfp,
                         eventPic: eventPic
                     )
+                    
                     self.delegate?.createdEvent(newEvent)
+                    //DONT NEED THE BOTTOM LINE OF CODE?
                     self.dismiss(animated: true)
                 }
             }
@@ -145,6 +204,7 @@ class Event {
     var startTime: Date
     var endTime: Date
     var location: String
+    var coordinate: GeoPoint?
     //each event can be sorted by specific activities, ex. fitness and soccer
     var activities: String
     var numPeople: Int
@@ -153,59 +213,19 @@ class Event {
     var eventPic: String
     //need RSVP list
     
-    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activities: String, numPeople: Int, hostUsername: String, hostPfp: String, eventPic: String) {
+    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, coordinate: GeoPoint?, activities: String, numPeople: Int, hostUsername: String, hostPfp: String, eventPic: String) {
         self.title = title
         self.description = description
         self.date = date
         self.startTime = startTime
         self.endTime = endTime
         self.location = location
+        self.coordinate = coordinate
         self.activities = activities
         self.numPeople = numPeople
         self.hostUsername = hostUsername
         self.hostPfp = hostPfp
         self.eventPic = eventPic
     }
-    
-//    func addActivity(_ activity: String) {
-//        //Check if activity exists in global list before adding to event
-//
-//    }
 }
 
-// Singleton class, global list of activities
-// Each event has its own associated list of activities
-// which pulls from the class
-//class allActivities {
-//    //single instance
-//    static let shared = allActivities()
-//
-//    //other classes cannot directly access this
-//    private(set) var globalActivities: [String] = ["Hiking", "Running"]
-//
-//    //initializer
-//    private init() {}
-//
-//    func addGlobalActivity(_ activity: String) {
-//        if !globalActivities.contains(activity) {
-//            globalActivities.append(activity)
-//            print("\(activity) has been added to the global list")
-//        } else {
-//            print("\(activity) already exists in the global list")
-//        }
-//    }
-//
-//    func removeGlobalActivity(_ activity: String) {
-//        if let index = globalActivities.firstIndex(of: activity) {
-//            globalActivities.remove(at: index)
-//            print("\(activity) has been removed from the global activity list")
-//        } else {
-//            print("\(activity) does not exist in global activity list")
-//        }
-//    }
-//    //print all activities in global list
-//    func printActivities() {
-//        let activitiesList = globalActivities.joined(separator: ", ")
-//            print("Global activities: \(activitiesList)")
-//    }
-//}
