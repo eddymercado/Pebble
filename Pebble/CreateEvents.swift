8d99a6e4776f26366d53a17fdd5f59c22910b83c
eddymercado <eddym@utexas.edu>
2024-11-12 21:51:59 -0600

    event pic saved now

--- Pebble/CreateEvents.swift
+++ Pebble/CreateEvents.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAnalytics
+import FirebaseFirestoreInternal
 
 protocol EventCreationDelegate: AnyObject {
     func createdEvent(_ event: Event)
@@ -37,32 +40,83 @@ class CreateEvents: UIViewController {
         present(vc, animated: true)
     }
     
+    func decodeBase64ToImage(_ base64String: String) -> UIImage? {
+        guard let imageData = Data(base64Encoded: base64String) else { return nil }
+        return UIImage(data: imageData)
+    }
     
     @IBAction func createEventBtnPressed(_ sender: UIButton) {
-        // Get selected date and times from date picker
-        let selectedDate = eventDate.date
-        let selectedStart = eventStartTime.date
-        let selectedEnd = eventEndTime.date
+        let db = Firestore.firestore()
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        
+        db.collection("users").document(userId).getDocument { (document, error) in
+            if let error = error {
+                print("Error retrieving document: \(error.localizedDescription)")
+                return
+            }
 
-        // Create a new instance of Event, populating inputs
-        let newEvent = Event(
-            title: eventTitle.text ?? "",
-            description: eventDesc.text ?? "",
-            date: selectedDate,
-            startTime: selectedStart,
-            endTime: selectedEnd,
-            location: eventLocation.text ?? "",
-            activities: eventActivityType.text ?? "",
-            numPeople: Int(eventNumPeople.text ?? "0") ?? 0
-        )
+            var hostUsername = "temp"
+            var hostPfp = "temp"
+            var eventPic = "temp"
+            
+            if let document = document, document.exists {
+                hostPfp = document.get("profilePic") as? String ?? "temp"
+                hostUsername = document.get("username") as? String ?? "temp"
+            } else {
+                print("Document does not exist")
+            }
+            
+            // Prepare event data for Firestore
+            let eventData: [String: Any] = [
+                "title": self.eventTitle.text ?? "",
+                "description": self.eventDesc.text ?? "",
+                "date": self.eventDate.date,
+                "startTime": self.eventStartTime.date,
+                "endTime": self.eventEndTime.date,
+                "location": self.eventLocation.text ?? "",
+                "activities": self.eventActivityType.text ?? "",
+                "numPeople": Int(self.eventNumPeople.text ?? "0") ?? 0,
+                "hostUsername": hostUsername,
+                "hostPfp": hostPfp,
+                "hostId": userId
+            ]
 
-        // Notify the delegate that a new event was created
-        delegate?.createdEvent(newEvent)
-
-        // Dismiss the modal view controller
-        dismiss(animated: true)
+            // Add event to "events" collection
+            db.collection("events").addDocument(data: eventData) { error in
+                if let error = error {
+                    print("error")
+                } else {
+                    if let image = self.eventImageView.image,
+                       let imageData = image.jpegData(compressionQuality: 0.5) {
+                        let base64String = imageData.base64EncodedString()
+                        eventPic = base64String
+                        Analytics.setUserProperty(base64String, forName: "Event Pic")
+                    }
+                    // Optional: Track analytics properties for the event
+                    Analytics.setUserProperty(hostUsername, forName: "Host Username")
+                    Analytics.setUserProperty(self.eventTitle.text ?? "", forName: "Event Title")
+                    Analytics.setUserProperty(self.eventLocation.text ?? "", forName: "Event Location")
+                    
+                    // Notify the delegate and dismiss the view
+                    let newEvent = Event(
+                        title: eventData["title"] as! String,
+                        description: eventData["description"] as! String,
+                        date: eventData["date"] as! Date,
+                        startTime: eventData["startTime"] as! Date,
+                        endTime: eventData["endTime"] as! Date,
+                        location: eventData["location"] as! String,
+                        activities: eventData["activities"] as! String,
+                        numPeople: eventData["numPeople"] as! Int,
+                        hostUsername: hostUsername,
+                        hostPfp: hostPfp,
+                        eventPic: eventPic
+                    )
+                    self.delegate?.createdEvent(newEvent)
+                    self.dismiss(animated: true)
+                }
+            }
+        }
     }
-
 
 }
 
@@ -92,8 +146,11 @@ class Event {
     //each event can be sorted by specific activities, ex. fitness and soccer
     var activities: String
     var numPeople: Int
+    var hostUsername: String
+    var hostPfp: String
+    var eventPic: String
     
-    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activities: String, numPeople: Int) {
+    init(title: String, description: String, date: Date, startTime: Date, endTime: Date, location: String, activities: String, numPeople: Int, hostUsername: String, hostPfp: String, eventPic: String) {
         self.title = title
         self.description = description
         self.date = date
@@ -102,6 +159,9 @@ class Event {
         self.location = location
         self.activities = activities
         self.numPeople = numPeople
+        self.hostUsername = hostUsername
+        self.hostPfp = hostPfp
+        self.eventPic = eventPic
     }
     
 //    func addActivity(_ activity: String) {
